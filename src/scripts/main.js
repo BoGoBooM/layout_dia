664137593b5c41b7f242a36d7150155f4c871780
Bogdan Pasokha <MyPikcha@gmail.com>
2022-10-08 20:17:23 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-document.querySelector('.contact-us__forms').addEventListener('submit',
-  function(event) {
-    event.preventDefault();
-    document.querySelector('.contact-us__forms').reset();
-  })
